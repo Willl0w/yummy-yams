11f6abd2b287c38e9d817dbc0a4b68b4961195d9
Loris Dié <lorisdie@gmail.com>
2024-05-08 11:36:17 +0200

    [api] add server

---
+++ api/src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import AuthRouter from "./routers/auth";
+import YamsRouter from "./routers/yams";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+const port = 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/auth", AuthRouter);
+app.use("/yams", YamsRouter);
+
+mongoose
+  .connect("mongodb://mongo/yams", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: "root",
+    pass: "foobar",
+  })
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
