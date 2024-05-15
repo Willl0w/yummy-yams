e6bf286956de5dd1d58143f3aa557168bb68a7b6
Loris Dié <lorisdie@gmail.com>
2024-05-15 13:19:50 +0200

    [app] add input label

--- app/src/components/Input.tsx
+++ app/src/components/Input.tsx
@@ -3,17 +3,22 @@ interface InputProps {
   value: string;
   onChange: (value: string) => void;
   placeholder: string;
+  label?: string;
 }
 
 export default function Input(props: InputProps) {
-  const { type, value, onChange, placeholder } = props;
+  const { type, value, onChange, placeholder, label } = props;
   return (
-    <input
-      className="border-2 border-gray-600 rounded-xl px-4 py-2"
-      type={type}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder={placeholder}
-    />
+    <div className="grid grid-rows">
+      <label>{label}</label>
+
+      <input
+        className="border-2 border-gray-600 rounded-xl px-4 py-2"
+        type={type}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+      />
+    </div>
   );
 }
